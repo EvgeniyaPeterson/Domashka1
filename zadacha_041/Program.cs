56f5d8c766ca6ff41c1403cddd902a4bef98db16
«Evgeniya» <e.madonova@gmail.com>
2022-12-05 19:34:22 +0300

    Задача 41,43

---
+++ zadacha_041/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int[] ArrayNumbers()
+{
+    Console.WriteLine("Введите длинну массива: ");
+    int lenght = int.Parse(Console.ReadLine()!);
+
+    int[] array = new int[lenght];
+
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void Print(int[] input)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        Console.Write(input[i]);
+        if (i != input.Length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
+
+int CountPositiveNumbers(int[] input)
+{
+    int result = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] > 0)
+            result++;
+    }
+    return result;
+}
+
+var array = ArrayNumbers();
+Print(array);
+
+var positivecount = CountPositiveNumbers(array);
+Console.WriteLine("Количество положительных числел = " + positivecount);
+
---
+++ zadacha_043/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку 
+//пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+//задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+int Koefficients()
+{
+    Console.Write("Введите к1= ");
+    int k1 = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите к2= ");
+    int k2 = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите b1= ");
+    int b1 = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите b2= ");
+    int b2 = int.Parse(Console.ReadLine()!);
+
+    return k1;
+    return k2;
+    return b1;
+    return b2;
+}
+
+double CoordinatesOfIntersectionPoint(double x, double y)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    return x;
+    return y;
+}
+
+void Print(int koeffic)
+{
+    Console.Write(k1);
+    Console.Write(k2);
+    Console.Write(b1);
+    Console.Write(b1);
+
+}
